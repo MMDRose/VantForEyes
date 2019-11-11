133fc0283bb62e3fbed44405396349257f1f050f
Rose <lrose4511@163.com>
2019-11-11 16:34:17 +0800

    UI 2.0 - center

--- vant-for-eyes-10.30/src/pages/Center.vue
+++ vant-for-eyes-10.30/src/pages/Center.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="center">
     <!--1. navbar 导航(公共一级)-->
-    <van-nav-bar class="navbar" title="" @click-right="onGetMsg">
-      <van-icon class="icon-info" name="comment-o" dot color="#fff"  slot="right" />
-    </van-nav-bar>
+    <van-nav-bar class="navbar" title=""></van-nav-bar>
 
     <!--2. 个人信息-->
     <header class="pb-30 w-100">
-      <!--未登录状态-->
+      <!--登录状态-->
       <div class="personal" v-if="ifLogin">
         <!--2.1 图片容器-->
         <div class="img-box">
@@ -15,12 +13,12 @@
         </div>
         <!--2.2 用户昵称-->
         <div class="name-box">
-          <h1 class="name">点击登录</h1>
+          <h1 class="name">小小小肥肥</h1>
         </div>
       </div>
 
-      <!--登录状态-->
-      <div class="personal" v-else>
+      <!--未登录状态-->
+      <div class="personal" v-else @click="getOriginMethod({'t':'0'},'onGetLogin')">
         <!--2.1 图片容器-->
         <div class="img-box">
           <img src="../assets/image/center/center-default-head.png" alt="">
@@ -32,54 +30,73 @@
       </div>
     </header>
 
+    <div class="transform-bg">
+      <img src="../assets/image/center/center-header-bg.png" alt="">
+    </div>
+
     <!--3. 主要功能-->
-    <main class="main-function pb-30 w-100">
+    <main class="main-function w-100">
       <div class="main-function-box">
-        <h1 class="function-title">我的服务</h1>
         <van-grid :border="false" :column-num="3">
-          <!--3.1 循环项，点击转原生方法-->
-          <van-grid-item @click="getOriginMethod({'t':'0'},item.getMethod)">
-            <van-image :src="item.image"></van-image>
-            <h2 class="func-name" v-text="item.title"></h2>
+          <van-grid-item @click="getOriginMethod({'t':'0'},'onGetPatient')">
+            <van-image :src="this.iconImg[0]"></van-image>
+            <h2 class="func-name">就诊人管理</h2>
+          </van-grid-item>
+          <van-grid-item @click="getOriginMethod({'t':'0'},'onGetArchives')">
+            <van-image :src="this.iconImg[1]"></van-image>
+            <h2 class="func-name">健康档案</h2>
+          </van-grid-item>
+          <!--订单跳转到H5订单列表-->
+          <van-grid-item @click="onGetOrderList">
+            <van-image :src="this.iconImg[2]"></van-image>
+            <h2 class="func-name">我的订单</h2>
           </van-grid-item>
         </van-grid>
       </div>
     </main>
 
     <!--4. 辅助功能-->
-    <div class="sub-function pb-30 w-100">
+    <div class="sub-function w-100">
       <ul class="w-100 sub-items">
+        <!--4.0 消息中心-->
+        <li class="item" @click="getOriginMethod('{\'t\': \'0\'}', 'onGetMsg')">
+          <h1>
+            <img class="center-sub-icon" src="../assets/image/center/center-sub-00.png" alt="消息中心">
+            <span class="center-sub-name">消息中心</span>
+          </h1>
+          <img class="enter" src="../assets/image/enter.png" alt="enter">
+        </li>
         <!--4.1 意见反馈-->
         <li class="item" @click="getOriginMethod('{\'t\': \'0\'}', 'onGetQA')">
           <h1>
-            <img class="center-sub-icon" src="../assets/image/center/center-sub-01.png" alt="">
+            <img class="center-sub-icon" src="../assets/image/center/center-sub-01.png" alt="意见反馈">
             <span class="center-sub-name">意见反馈</span>
           </h1>
-          <img class="enter" src="../assets/image/enter.png" alt="">
+          <img class="enter" src="../assets/image/enter.png" alt="enter">
         </li>
         <!--4.2 隐私条款-->
         <li class="item" @click="getOriginMethod('{\'t\': \'0\'}', 'onGetPolicy')">
           <h1>
-            <img class="center-sub-icon" src="../assets/image/center/center-sub-02.png" alt="">
+            <img class="center-sub-icon" src="../assets/image/center/center-sub-02.png" alt="隐私条款">
             <span class="center-sub-name">隐私条款</span>
           </h1>
-          <img class="enter" src="../assets/image/enter.png" alt="">
+          <img class="enter" src="../assets/image/enter.png" alt="enter">
         </li>
         <!--4.3 关于我们-->
         <li class="item" @click="getOriginMethod('{\'t\': \'0\'}', 'onGetAbout')">
           <h1>
-            <img class="center-sub-icon" src="../assets/image/center/center-sub-03.png" alt="">
+            <img class="center-sub-icon" src="../assets/image/center/center-sub-03.png" alt="关于我们">
             <span class="center-sub-name">关于我们</span>
           </h1>
-          <img class="enter" src="../assets/image/enter.png" alt="">
+          <img class="enter" src="../assets/image/enter.png" alt="enter">
         </li>
         <!--4.4 设置-->
         <li class="item"  @click="getOriginMethod('{\'t\': \'0\'}', 'onGetSetting')">
           <h1>
-            <img class="center-sub-icon" src="../assets/image/center/center-sub-04.png" alt="">
+            <img class="center-sub-icon" src="../assets/image/center/center-sub-04.png" alt="设置">
             <span class="center-sub-name">设置</span>
           </h1>
-          <img class="enter" src="../assets/image/enter.png" alt="">
+          <img class="enter" src="../assets/image/enter.png" alt="enter">
         </li>
       </ul>
     </div>
@@ -91,33 +108,22 @@ export default {
   name: 'center',
   data () {
     return {
-      funcList: [
+      'ifLogin': false, // 是否登录状态（默认false未登录）
+      'iconImg': [ // 图片引入
         require('../assets/image/center/center-func-icon01.png'),
         require('../assets/image/center/center-func-icon02.png'),
         require('../assets/image/center/center-func-icon03.png')
-      ],
-      functionList: [{
-        'title': '就诊人管理',
-        'image': require('../assets/image/center/center-func-icon01.png'),
-        'getMethod': ''
-      }, {
-        'title': '健康档案',
-        'image': require('../assets/image/center/center-func-icon02.png'),
-        'getMethod': ''
-      }, {
-        'title': '我的订单',
-        'image': require('../assets/image/center/center-func-icon03.png'),
-        'getMethod': ''
-      }]
+      ]
     }
   },
   methods: {
     // 点击获取通知
     onGetMsg () {
-      alert('hahaha')
+      alert('点击获取通知')
     },
-    test () {
-      this.getOriginMethod('参数', '方法')
+    // 关于订单
+    onGetOrderList () {
+      alert('订单处理')
     }
   }
 }
@@ -130,17 +136,19 @@ export default {
     border none
 
   .main-function >>> .van-image
-    width 1.1rem
-    height 1.16rem
+    width .9rem
+    height .9rem
     img
       max-width 100%
       width 100%
-
+.center
+  position relative
   /*1. navbar 导航*/
   .navbar
+    position relative
     height .88rem
     line-height .88rem
-    background-color #14CCCC
+    background-color $bgMainBlue
     /*navbar 标题*/
     .van-nav-bar__title
       color #fff
@@ -148,20 +156,17 @@ export default {
   /*2. 个人信息展示*/
   header
     position relative
-    top: .88rem
+    height 1.98rem
     width 100%
-    height auto
-    min-height 3.28rem
-    background-color red
-    background url("../assets/image/center/center-header-bg.png") no-repeat
-    background-size 100% auto
+    text-align center
+    background-color $bgMainBlue
     /*个人信息内容*/
     .personal
       width 100%
       height 1.2rem
       /*2.1 图片容器*/
       .img-box
-        float left
+        margin 0 auto
         width 1.2rem
         height 1.2rem
         border-radius 50%
@@ -169,55 +174,55 @@ export default {
           max-width 100%
       /*2.2 用户昵称*/
       .name-box
-        float left
-        margin-left .16rem
-        width 2.8rem
-        height 1.2rem
-        line-height 1.2rem
+        margin-top .08rem
+        width 100%
+        height .5rem
+        line-height .5rem
+        overflow hidden
         .name
-          width 2.8rem
-          ellipsis()
-          font-size .48rem
+          font-size .36rem
           font-weight 500
           color $colorWhite
 
-  /*3. 主要内容*/
+  /*过度背景*/
+  .transform-bg
+    position absolute
+    top 2.78rem
+    width 100%
+    height auto
+    img
+      width 100%
+      height auto
+      max-width 100%
+      border none
+
+/*3. 主要内容*/
   .main-function
+    padding auto .8rem
+    box-sizing border-box
     position relative
-    margin-top -.8rem
-    /*padding-top .2rem*/
-    background transparent
+    top: .34rem
+    height 1.98rem
     .main-function-box
       overflow hidden
+      text-align center
       width 100%
-      height 2.6rem
-      box-shadow 0 .04rem .12rem 0 rgba(0,0,0,0.1)
-      background-color $bgWhite
-      border-radius $borderRadius8
-      /*3.1 标题*/
-      .function-title
-        width 100%
-        height .8rem
-        line-height .8rem
-        padding 0 .16rem
-        box-sizing border-box
-        font-size .32rem
-        font-weight 400
-        color $textMainTitle
-        border-bottom .01rem solid $borderColorEE
+      height 1.98rem
       .van-grid
         .func-name
-          margin-top -.12rem
+          margin-top .16rem
           color $textSub
           font-weight 400
           font-size .2rem
 
   /*4. 辅助内容列表*/
   .sub-function
-    margin-top .64rem
+    margin-top .32rem
     .sub-items
       /*li*/
       .item
+        padding 0 .3rem
+        box-sizing border-box
         display flex
         align-items center
         justify-content space-between
@@ -240,4 +245,9 @@ export default {
         .enter
           width .24rem
           height .24rem
+      .item:first-child
+        height 1.48rem
+        border-top .24rem solid $borderColorEE
+        border-bottom .24rem solid $borderColorEE
+
 </style>
